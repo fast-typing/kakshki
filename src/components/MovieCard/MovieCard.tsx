90d342c8df715a162287357c3839998e26a0fa7e
dobriy <82109250+fast-typing@users.noreply.github.com>
2023-10-19 23:02:19 +0400

    D)SA*(UHAS*9dij 9asdj 90as8dj _)*IYAW a     asd

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,40 +1,63 @@
 import React, { useState } from "react";
 import { Movie } from "../../interfaces/Interfaces";
-import StarIcon from '@mui/icons-material/Star';
-import './MovieCard.css'
+import StarIcon from "@mui/icons-material/Star";
+import "./MovieCard.css";
 import { Chip } from "@mui/material";
-import IconButton from '@mui/material/IconButton';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import IconButton from "@mui/material/IconButton";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import { useNavigate } from "react-router-dom";
 
 export default function MovieCard(props: { movie: Movie }) {
-    const [isFavorite, setIsFavorite] = useState(false)
+  const [isFavorite, setIsFavorite] = useState(false);
+  const navigate = useNavigate();
 
-    function toGenre() { }
+  function toGenre(genre: string) {
+    navigate(`/search?genre=${genre}`);
+  }
 
-    function getGenres() { return props.movie.genres.map((genre) => <Chip onClick={toGenre} label={genre} variant="outlined" />) }
+  function getGenres() {
+    return props.movie.genres.map((genre) => (
+      <Chip onClick={() => toGenre(genre)} label={genre} variant="outlined" />
+    ));
+  }
 
-    function toggleFavorite(): void { setIsFavorite(!isFavorite) }
+  function toggleFavorite(): void {
+    setIsFavorite(!isFavorite);
+  }
 
-    return (
-        <div className="card">
-            <div className="float-icon">
-                <IconButton onClick={toggleFavorite} color={isFavorite ? "secondary" : "success"}>
-                    {isFavorite ? <BookmarkIcon /> : <BookmarkBorderIcon />}
-                </IconButton>
-            </div>
-            <img src='https://www.iephb.ru/wp-content/uploads/2021/01/img-placeholder.png' alt={props.movie.poster} />
-            {/* <img src={props.movie.poster} alt={props.movie.poster} /> */}
-            <div className="flex justify-between items-center">
-                <h2>{props.movie.title}</h2>
-                <div className="flex">
-                    <StarIcon />
-                    <span>{props.movie.average_rating}</span>
-                </div>
-            </div>
-            <div className="flex gap-2">
-                {getGenres()}
-            </div>
+  const routeToMovie = () => {
+    navigate(`/movie/${props.movie.id}`);
+  };
+
+  return (
+    <div className="card">
+      <div className="grid gap-1 cursor-pointer" onClick={routeToMovie}>
+        <div className="float-icon">
+          <IconButton
+            onClick={toggleFavorite}
+            color={isFavorite ? "secondary" : "success"}
+          >
+            {isFavorite ? <BookmarkIcon /> : <BookmarkBorderIcon />}
+          </IconButton>
         </div>
-    )
+        <img
+          src={
+            props.movie.poster == "string"
+              ? "https://www.iephb.ru/wp-content/uploads/2021/01/img-placeholder.png"
+              : props.movie.poster
+          }
+          alt={props.movie.poster}
+        />
+        <div className="flex justify-between items-center">
+          <h2>{props.movie.title}</h2>
+          <div className="flex">
+            <StarIcon />
+            <span>{props.movie.average_rating}</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-2">{getGenres()}</div>
+    </div>
+  );
 }
